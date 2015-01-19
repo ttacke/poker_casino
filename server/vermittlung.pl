27bae07c920017fec1481ee681be0f1b909d1f4b
ttacke <ttacke@e6676588-be30-0410-be5a-a11ef2dab0a3>
2015-01-19 08:15:12 +0000

    Verbindungsprotokoll begonnen
    git-svn-id: https://svn.apps.seibert-media.net/repos/main/ci_job_utils/supermaexchen@2636 e6676588-be30-0410-be5a-a11ef2dab0a3

--- server/vermittlung.pl
+++ server/vermittlung.pl
@@ -1,66 +1,20 @@
 use strict;
 use lib './lib';
+
 use Net::WebSocket::Server;
+use LOCAL::Vermittlung();
 
-my $MESSAGES_TO = {
-};
-use Data::Dumper();
-
-BEGIN { $ENV{PERL_JSON_BACKEND} = 'JSON::PP' }
-use JSON -support_by_pp, -no_export;
-
-my $json = JSON->new();
-my $connections = [];
-
+my $vermittlung = LOCAL::Vermittlung->new();
 Net::WebSocket::Server->new(
 	listen => 8080,
 	on_connect => sub {
 		my ($serv, $conn) = @_;
 		
-		# TODO Connection zu playerId zuordnen!
-		# from, to, msg reichen aus, dann ist socket fertig
-		# der rest wird vom server gemacht, der tacktet und steuert
-		push(@$connections, $conn);
-		
-		# normaler ChildRequest
+		$vermittlung->neueVerbindung($conn);
 		$conn->on(
 			utf8 => sub {
 				my ($conn, $msg) = @_;
-				
-				
-				my $data = eval {
-					return $json->utf8->decode($msg);
-				};
-				if($data) {
-					#if($data->{'from'} eq 's') {
-						foreach my $c (@$connections) {
-							next if($c == $conn);
-							
-							# Verbindung gerade verfuegbar?
-							my $fh = $c->socket();
-							if(tell($fh) != -1) {
-								$c->send_utf8($data->{'msg'});
-							}
-						}
-						$MESSAGES_TO->{$data->{'to'}} = '';
-					#}
-				}
-				
-				# TODO fehler immer abweisen!
-				# 
-				# Server sagt spieler 1 PING1
-				# Spieler 1 kriegt PING1 und sagt PONG1
-				# Spieler 2 bekommt keine Nachricht
-				# Server kriegt PONG1
-				
-				# Server sagt spieler 2 PING2
-				# Spieler 2 kriegt PING2 und sagt PONG2
-				# Spieler 1 bekommt keine Nachricht
-				# Server kriegt PONG2
-				
-				#$DATA->{$msg}++;
-				#warn Data::Dumper::Dumper($DATA);
-				#$conn->send_utf8("ENDE");
+				$vermittlung->neueAnfrage($conn, $msg);
 			},
 		);
 	},
