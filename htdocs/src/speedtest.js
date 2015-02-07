8e9e9715992ff7fdac5cb97f5204b2e2ed3be0fe
ttacke <ttacke@e6676588-be30-0410-be5a-a11ef2dab0a3>
2015-02-07 01:25:50 +0000

    Aufgeraeumt
    git-svn-id: https://svn.apps.seibert-media.net/repos/main/ci_job_utils/casino@2675 e6676588-be30-0410-be5a-a11ef2dab0a3

--- htdocs/src/speedtest.js
+++ htdocs/src/speedtest.js
@@ -1,10 +1,38 @@
 var listeAllerAbfragen = [];
 var aktuelleLogEintragungen = {};
+var listeAllerSpielerWerte = {};
 
 $(function() {
 	setInterval('schreibeLog()', 1000);
 });
 
+// VOID
+function fuegeSpielerWertEin(spielerNr, art, wert) {
+	if(!listeAllerSpielerWerte[spielerNr]) {
+		listeAllerSpielerWerte[spielerNr] = [];
+	}
+	
+	listeAllerSpielerWerte[spielerNr].push({ art: art, wert: wert });
+	while(listeAllerSpielerWerte[spielerNr].length > top.window.maxAnzahlFuerAuswertungen) {
+		listeAllerSpielerWerte[spielerNr].shift();
+	}
+}
+// INT
+function gibSpielerWert(spielerNr, art) {
+	if(!listeAllerSpielerWerte[spielerNr]) return 0;
+	
+	var summe = 0;
+	listeAllerSpielerWerte[spielerNr].map(function(obj) { if(obj.art == art) summe += obj.wert; });
+	return summe;
+}
+// INT
+function gibGesamtWert(art) {
+	var summe = 0;
+	for(spielerNr in listeAllerSpielerWerte) {
+		listeAllerSpielerWerte[spielerNr].map(function(obj) { if(obj.art == art) summe += obj.wert; });
+	}
+	return summe;
+}
 // INT
 function ermittleGeschwindigkeit() {
 	var jetzt = new Date().getTime();
