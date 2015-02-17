32de7278f2f108392a7cf83e69e22fb585ff5d32
ttacke <ttacke@e6676588-be30-0410-be5a-a11ef2dab0a3>
2015-02-17 00:40:30 +0000

    ErmittleGewinner weiter umgesetzt, neue Tests eingefügt
    git-svn-id: https://svn.apps.seibert-media.net/repos/main/ci_job_utils/casino@2688 e6676588-be30-0410-be5a-a11ef2dab0a3

--- htdocs/spec/TexasHoldEmLimitedPoker.js
+++ htdocs/spec/TexasHoldEmLimitedPoker.js
@@ -11,14 +11,14 @@ describe("Szenario: das Casino ist geöffnet", function() {
 				schlaegt: function(b) {
 					var nameB = b.substr(0, b.indexOf(" "));
 					var blattB = b.substr(b.indexOf(" ") + 1, b.length);
-					if(!c.blattAschlaegtB(blattA, blattB)) {
+					if(!c.blattAschlaegtB(blattA, blattB) || c.blattAschlaegtB(blattB, blattA)) {
 						expect(nameA + " > " + nameB).toBe(a + " schlaegt " + b);
 					}
 				},
 				unterliegt: function(b) {
 					var nameB = b.substr(0, b.indexOf(" "));
 					var blattB = b.substr(b.indexOf(" ") + 1, b.length);
-					if(c.blattAschlaegtB(blattA, blattB)) {
+					if(c.blattAschlaegtB(blattA, blattB) || !c.blattAschlaegtB(blattB, blattA)) {
 						expect(nameA + " < " + nameB).toBe(a + " unterliegt " + b);
 					}
 				},
@@ -38,10 +38,13 @@ describe("Szenario: das Casino ist geöffnet", function() {
 			it("String in Karten konvertieren", function() {
 				var k = c.parseKarten("K♥ 10♣");
 				expect(k.length).toBe(2);
-				expect(k[0].wert).toBe("K");
+				expect(k[0].bezeichnung).toBe("K");
 				expect(k[0].farbe).toBe("♥");
-				expect(k[1].wert).toBe("10");
+				expect(k[0].zahlwert).toBe(13);
+				
+				expect(k[1].bezeichnung).toBe("10");
 				expect(k[1].farbe).toBe("♣");
+				expect(k[1].zahlwert).toBe(10);
 			});
 		});
 		it("Blatt-Bewertung", function() {
@@ -49,7 +52,7 @@ describe("Szenario: das Casino ist geöffnet", function() {
 			//http://de.wikipedia.org/wiki/Hand_%28Poker%29
 			blatt("HighCard A♦ 10♦ 9♠ 5♣ 4♣").schlaegt("HighCard K♣ Q♦ J♣ 8♥ 7♥");
 			blatt("HighCard A♦ 10♦ 9♠ 5♣ 4♣").schlaegt("HighCard A♣ 9♦ 8♥ 5♠ 4♠");
-			blatt("HighCard Q♣ J♣").unterliegt("HighCard K♦ 10♦");
+			blatt("HighCard Q♣ J♣ 5♣").unterliegt("HighCard K♦ 10♦ 5♣");
 			
 			blatt("EinPaar 9♥ 9♣ 2♥ Q♦ 10♦").schlaegt("HighCard A♦ Q♦ 9♠ 10♣ 4♣");
 			
@@ -58,7 +61,6 @@ describe("Szenario: das Casino ist geöffnet", function() {
 			blatt("EinPaar 10♥ 10♦ J♦ 4♥ 3♣").schlaegt("EinPaar 10♣ 10♠ J♠ 4♦ 2♥");
 			
 			blatt("ZweiPaare  7♦ 7♠ 10♠ 10♣ 9♠").schlaegt("EinPaar 10♥ 10♦ J♦ 4♥ 3♣");
-			return;//TODO
 			blatt("ZweiPaare K♥ K♦ 2♣ 2♦ J♥").schlaegt("ZweiPaare  J♦ J♠ 10♠ 10♣ 9♠");
 			blatt("ZweiPaare 4♠ 4♣ 3♠ 3♥ K♦").schlaegt("ZweiPaare 4♥ 4♦ 3♦ 3♣ 10♠");
 			
@@ -68,13 +70,17 @@ describe("Szenario: das Casino ist geöffnet", function() {
 			blatt("Drilling 8♠ 8♥ 8♦ A♣ 2♦").schlaegt("Drilling 8♣ 8♥ 8♦ 5♠ 3♣");
 			
 			blatt("Straight 6♦ 5♠ 4♦ 3♥ 2♣").schlaegt("Drilling 8♠ 8♥ 8♦ A♣ 2♦");
+	
+			blatt("Straight 5♠ 4♠ 3♥ 2♥ A♠").schlaegt("Drilling 8♠ 8♥ 8♦ A♣ 2♦");
 			
 			blatt("Straight 8♠ 7♠ 6♥ 5♥ 4♠").schlaegt("Straight 6♦ 5♠ 4♦ 3♥ 2♣");
 			blatt("Straight 8♠ 7♠ 6♥ 5♥ 4♠").splitted("Straight 8♥ 7♦ 6♣ 5♣ 4♥");
 			
-			blatt("Flush ♠ Q♠ J♠ 9♠ 6♠").schlaegt("Straight 8♠ 7♠ 6♥ 5♥ 4♠");
+			return;//TODO
 			
-			blatt("Flush A♥ Q♥ 10♥ 5♥ 3♥").schlaegt("Flush ♠ Q♠ J♠ 9♠ 6♠");
+			blatt("Flush K♠ Q♠ J♠ 9♠ 6♠").schlaegt("Straight 8♠ 7♠ 6♥ 5♥ 4♠");
+			
+			blatt("Flush A♥ Q♥ 10♥ 5♥ 3♥").schlaegt("Flush K♠ Q♠ J♠ 9♠ 6♠");
 			blatt("Flush A♦ K♦ 7♦ 6♦ 2♦").schlaegt("Flush A♥ Q♥ 10♥ 5♥ 3♥");
 			blatt("Flush Q♥ 10♥ 9♥ 5♥ 2♥").splitted("Flush Q♠ 10♠ 9♠ 5♠ 2♠");
 			
--- htdocs/src/Casino/Croupier/TexasHoldEmLimitedPoker.js
+++ htdocs/src/Casino/Croupier/TexasHoldEmLimitedPoker.js
@@ -1,15 +1,16 @@
 "use strict";
 
-function Spielkarte(wert, farbe) {
-	this.wert = wert;
+function Spielkarte(bezeichnung, farbe) {
+	this._berechne_zahlwert = function(bezeichnung) {
+		if(this.bezeichnung == 'A') return 14;
+		if(this.bezeichnung == 'K') return 13;
+		if(this.bezeichnung == 'Q') return 12;
+		if(this.bezeichnung == 'J') return 11;
+		return this.bezeichnung * 1;
+	};
+	this.bezeichnung = bezeichnung;
 	this.farbe = farbe;
-	this.punkte = function() {
-		if(this.wert == 'A') return 14;
-		if(this.wert == 'K') return 13;
-		if(this.wert == 'Q') return 12;
-		if(this.wert == 'J') return 11;
-		return this.wert * 1;
-	}
+	this.zahlwert = this._berechne_zahlwert(bezeichnung);
 }
 // CLASS DEFINITION
 CasinoCroupierTexasHoldEmLimitedPoker.prototype = new CasinoCroupier();
@@ -22,65 +23,133 @@ function CasinoCroupierTexasHoldEmLimitedPoker(name, passwort) {
 		var A = this.parseKarten(a);
 		var B = this.parseKarten(b);
 		
-		if(this._einPaarAschlaegtB(A, B)) return true;
-		if(this._highCardAschlaegtB(A, B)) return true;
-		return false;
-	};
-	
-	
-	
-	this._einPaarAschlaegtB = function(a, b) {
-		var punkteA = this._gibEinPaarPunkte(a);
-		var punkteB = this._gibEinPaarPunkte(b);
+		//console.log(a + " - " + b);
+		var punkteA = this._ermittlePunkteFuerBlattA(A, B);
+		var punkteB = this._ermittlePunkteFuerBlattA(B, A);
+		
+		//console.log(punkteA + " > " + punkteB);
 		if(punkteA > punkteB) return true;
 		return false;
 	};
-	this._gibEinPaarPunkte = function(blatt) {
-		var liste = [];
-		for(var i = 0; i < blatt.length; i++) {
-			liste.push(blatt[i].punkte());
-		}
-		liste = liste.sort(function(a, b) { return b - a; });
-		var letztePunkte = 0;
-		for(var i = 0; i < liste.length; i++) {
-			if(letztePunkte == liste[i]) {
-				return liste[i];
-			}
-			letztePunkte = liste[i];
-		}
+	// INT
+	this._ermittlePunkteFuerBlattA = function(A, B) {
+		//RoyalFlush
+		//StraightFlush
+		//if(this._fullHouseAschlaegtB(A, B)) return 4;
+		//if(this._flushAschlaegtB(A, B)) return 5;
+		if(this._straightAschlaegtB(A, B)) return 4;
+		if(this._drillingAschlaegtB(A, B)) return 3;
+		if(this._paerchenAschlaegtB(A, B)) return 2;
+		if(this._highCardAschlaegtB(A, B)) return 1;
 		return 0;
+	}
+	// BOOLEAN
+	this._straightAschlaegtB = function(a, b) {
+		var punkteA = this._gibStraightPunkte(a);
+		var punkteB = this._gibStraightPunkte(b);
+		if(punkteA > punkteB) return true;
+		return false;
 	};
 	
+	// INT
+	this._gibStraightPunkte = function(blatt) {
+		blatt = this._sortiereKartenHoechsteZuerst(blatt);
+		var punkte = this._gibNormaleStraightPunkte(blatt);
+		if(!punkte && blatt[0].bezeichnung == 'A') {
+			var assStraightBlatt = [];
+			for(var i = 1; i < blatt.length; i++) {
+				assStraightBlatt.push(blatt[i]);
+			}
+			assStraightBlatt.push(new Spielkarte('1', '?'));
+			punkte = this._gibNormaleStraightPunkte(assStraightBlatt);
+		}
+		return punkte;
+	}
 	
-	
+	this._gibNormaleStraightPunkte = function(blatt) {
+		var vorherigeKarte = null;
+		for(var i = 0; i < blatt.length; i++) {
+			var karte = blatt[i];
+			if(vorherigeKarte) {
+				if(vorherigeKarte.zahlwert != karte.zahlwert + 1) {
+					return 0;
+				}
+			}
+			vorherigeKarte = karte;
+		}
+		return blatt[0].zahlwert * 1;
+	}
+	// BOOLEAN
+	this._drillingAschlaegtB = function(a, b) {
+		return this._mehrlingAschlaegtB(a, b, 3);
+	};
+	// BOOLEAN
+	this._paerchenAschlaegtB = function(a, b) {
+		return this._mehrlingAschlaegtB(a, b, 2);
+	}
+	// BOOLEAN
+	this._mehrlingAschlaegtB = function(a, b, gesuchte_mehrling_anzahl) {
+		var paareA = this._sortiereKartenHoechsteZuerst(this._gibWertMehrlingKarten(a, gesuchte_mehrling_anzahl));
+		var paareB = this._sortiereKartenHoechsteZuerst(this._gibWertMehrlingKarten(b, gesuchte_mehrling_anzahl));
+		for(var i = 0; i < paareA.length; i++) {
+			var paarKarteA = paareA[i];
+			var paarKarteB = paareB[i];
+			
+			if(paarKarteA && !paarKarteB) return true;
+			if(!paarKarteA && paarKarteB) return false;
+			
+			if(paarKarteA.zahlwert > paarKarteB.zahlwert) return true;
+			if(paarKarteA.zahlwert < paarKarteB.zahlwert) return false;
+		}
+		return false;
+	};
+	// INT
+	this._gibWertMehrlingKarten = function(blatt, gesuchte_mehrling_anzahl) {
+		var mehrlinge = this._gruppiereZahlwerte(blatt);
+		var gefundene_mehrlinge = [];
+		for(var zahlwert in mehrlinge) {
+			if(mehrlinge[zahlwert].length == gesuchte_mehrling_anzahl) {
+				gefundene_mehrlinge.push(mehrlinge[zahlwert][0]);
+			}
+		}
+		return gefundene_mehrlinge;
+	};
+	// HASH
+	this._gruppiereZahlwerte = function(blatt) {
+		var zahlwerte = {};
+		for(var i = 0; i < blatt.length; i++) {
+			var karte = blatt[i];
+			if(!zahlwerte[karte.zahlwert]) {
+				zahlwerte[karte.zahlwert] = [];
+			}
+			zahlwerte[karte.zahlwert].push(karte);
+		}
+		return zahlwerte;
+	}
+	// BOOLEAN
 	this._highCardAschlaegtB = function(a, b) {
-		var highCardA = this._gibAlleHighCardPunkte(a);
-		var highCardB = this._gibAlleHighCardPunkte(b);
-		while(highCardA.length > 0) {
-			var punkteA = highCardA.shift();
-			var punkteB = highCardB.shift();
-			if(punkteA > punkteB) return true;
-			if(punkteA < punkteB) return false;
+		a = this._sortiereKartenHoechsteZuerst(a);
+		b = this._sortiereKartenHoechsteZuerst(b);
+		for(var i = 0; i < a.length; i++) {
+			var karteA = a[i];
+			var karteB = b[i];
+		
+			if(karteA.zahlwert > karteB.zahlwert) return true;
+			if(karteA.zahlwert < karteB.zahlwert) return false;
 		}
 		return false;
 	}
 	// ARRAY
-	this._gibAlleHighCardPunkte = function(blatt) {
-		var liste = [];
-		for(var i = 0; i < blatt.length; i++) {
-			liste.push(blatt[i].punkte());
-		}
-		liste = liste.sort(function(a, b) { return b - a; });
-		return liste;
+	this._sortiereKartenHoechsteZuerst = function(blatt) {
+		return blatt.sort(function(a, b) { return b.zahlwert - a.zahlwert; });
 	};
-	
-	
-	
 	// ARRAY
 	this.parseKarten = function(string) {
 		var stapel = [];
 		var liste = string.split(" ");
 		for(var i = 0; i < liste.length; i++) {
+			if(liste[i].length < 2) continue;
+			
 			var kartenDaten = liste[i].split("");
 			if(kartenDaten.length == 3) {
 				kartenDaten[0] += kartenDaten[1];
