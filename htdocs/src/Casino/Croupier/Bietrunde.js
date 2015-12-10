655e4e333b431434c52dcfad0ec13e29d9e5722b
ttacke <ttacke@e6676588-be30-0410-be5a-a11ef2dab0a3>
2015-12-10 19:07:01 +0000

    Bietrunde extrahiert
    git-svn-id: https://svn.apps.seibert-media.net/repos/main/ci_job_utils/casino@2862 e6676588-be30-0410-be5a-a11ef2dab0a3

---
+++ htdocs/src/Casino/Croupier/Bietrunde.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// CLASS DEFINITION
+function CasinoCroupierBietrunde(croupier, smallBlind) {
+	this.smallBlind = smallBlind;
+	this.croupier = croupier;
+	
+	// VOID
+	this.wetten = function(spielerrunde, kartenstapel, naechsteRunde) {
+		
+		this._ermittleEinsaetzeVonAllen(spielerrunde, naechsteRunde);
+	};
+	// VOID
+	this._ermittleEinsaetzeVonAllen = function(spielerrunde, doneFunc) {
+		this._ermittleDenEinsatz(doneFunc, spielerrunde, spielerrunde.anzahlDerSpieler());
+		
+	};
+	// VOID
+	this._ermittleDenEinsatz = function(doneFunc, spielerrunde, temp) {
+		if(temp <= 0) {//TODO implementieren
+			doneFunc(true);
+			return;
+		}
+			
+		var spieler = spielerrunde.gibDenSpielerDerAnDerReiheIst();
+		var frage = this.croupier._clone(this.croupier._gibSpielerdaten(spieler));
+		frage['Pot'] = this.croupier.pot + '';
+		frage['Stack'] = this.croupier.stack[spieler] + '';
+		frage['Hoechsteinsatz'] = this.croupier._gibAktuellenHoechsteinsatz() + '';
+		
+		var alle_spieler = spielerrunde.gibListe();
+		frage['Spieler'] = [];
+		for(var ii = 0; ii < alle_spieler.length; ii++) {
+			var einsatz = this.croupier._gibSpielerdaten(alle_spieler[ii])['Einsatz'];
+			frage['Spieler'].push({
+				'Name':alle_spieler[ii],
+				'letzteAktion':this.croupier._gibSpielerdaten(alle_spieler[ii])['letzteAktion'],
+				'Stack':this.croupier.stack[alle_spieler[ii]] + '',
+				'Einsatz':einsatz,
+			});
+		}
+		var self = this;
+		this.croupier.frageDenSpieler(
+			spieler,
+			frage,
+			function(antwort) {
+				var aktion = self.croupier._uebersetzeAntwort(antwort);
+				if(aktion == 'check') {
+					var hoechsteinsatz = self.croupier._gibAktuellenHoechsteinsatz();
+					self.croupier._erhoeheAuf(spieler, hoechsteinsatz);
+				}
+				self.croupier._speichereLetzteAktion(spieler, aktion);
+				self.croupier._ermittleDenEinsatz(doneFunc, temp - 1);
+			}
+		);
+	};
+}
--- htdocs/src/Casino/Croupier/Runde/PreFlop.js
+++ htdocs/src/Casino/Croupier/Runde/PreFlop.js
@@ -1,26 +1,24 @@
 "use strict";
 
 // CLASS DEFINITION
+CasinoCroupierRundePreflop.prototype = new CasinoCroupierBietrunde();
 function CasinoCroupierRundePreflop(croupier, smallBlind) {
 	this.smallBlind = smallBlind;
+	this.croupier = croupier;
+	
 	// VOID
 	this.vorbereiten = function(spielerrunde, kartenstapel) {
 		spielerrunde.starteWiederAbGeberToken();
 		
 		var smallBlindSpieler = spielerrunde.gibDenSpielerDerAnDerReiheIst();
-		croupier._erhoeheAuf(smallBlindSpieler, this.smallBlind + '');
+		this.croupier._erhoeheAuf(smallBlindSpieler, this.smallBlind + '');
 		//TODO intern!
 		
 		var bigBlindSpieler = spielerrunde.gibDenSpielerDerAnDerReiheIst();
-		croupier._erhoeheAuf(bigBlindSpieler, (this.smallBlind * 2) + '');
+		this.croupier._erhoeheAuf(bigBlindSpieler, (this.smallBlind * 2) + '');
 		//TODO intern!
 		
-		croupier._gibHandkartenAnAlleSpieler(2, kartenstapel);
+		this.croupier._gibHandkartenAnAlleSpieler(2, kartenstapel);
 		
-	};
-	// VOID
-	this.wetten = function(spielerrunde, kartenstapel, naechsteRunde) {
-		
-		croupier._ermittleEinsaetzeVonAllen(naechsteRunde);
 	};
 }
