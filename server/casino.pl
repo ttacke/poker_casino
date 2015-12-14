eb1ada4b8a0ae6e5574e7bdd94766f3cf4ac92f0
ttacke <ttacke@e6676588-be30-0410-be5a-a11ef2dab0a3>
2015-12-14 08:28:13 +0000

    Server kontinuierlich neu gestartet, wenn er abfällt
    git-svn-id: https://svn.apps.seibert-media.net/repos/main/ci_job_utils/casino@2973 e6676588-be30-0410-be5a-a11ef2dab0a3

--- server/casino.pl
+++ server/casino.pl
@@ -6,22 +6,25 @@ use LOCAL::Casino();
 
 my $IP = $ARGV[0] || '127.0.0.1';
 my $PORT = $ARGV[1] || '8080';
-warn "Starte Server auf $IP:$PORT ...";
 
-my $vermittlung = LOCAL::Casino->new();
-Net::WebSocket::Server->new(
-	listen => 8080,
-	on_connect => sub {
-		my ($serv, $conn) = @_;
-		
-		$vermittlung->neueVerbindung($conn);
-		$conn->on(
-			utf8 => sub {
-				my ($conn, $msg) = @_;
-				$vermittlung->neueNachricht($conn, $msg);
-			},
-		);
-	},
-)->start;
-
-warn "Server laeuft";
+while(1) {
+	warn "Starte Server auf $IP:$PORT ...";
+	
+	my $vermittlung = LOCAL::Casino->new();
+	Net::WebSocket::Server->new(
+		listen => 8080,
+		on_connect => sub {
+			my ($serv, $conn) = @_;
+			
+			$vermittlung->neueVerbindung($conn);
+			$conn->on(
+				utf8 => sub {
+					my ($conn, $msg) = @_;
+					$vermittlung->neueNachricht($conn, $msg);
+				},
+			);
+		},
+	)->start;
+	
+	warn "Server gestorben, neustart!";
+}
