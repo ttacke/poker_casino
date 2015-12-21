8949b9318c93ed55ea44cb2b1a08d9827e1b8434
ttacke <ttacke@e6676588-be30-0410-be5a-a11ef2dab0a3>
2015-12-21 22:12:19 +0000

    Inline-Zeug ausgelagert
    git-svn-id: https://svn.apps.seibert-media.net/repos/main/ci_job_utils/casino@3008 e6676588-be30-0410-be5a-a11ef2dab0a3

--- htdocs/src/casino.js
+++ htdocs/src/casino.js
@@ -9,3 +9,15 @@ function anzeige_verkleinern() {
 	anzeige_groesse -= 10;
 	$('html').attr('style', 'font-size: ' + anzeige_groesse + '%');
 }
+$(function() {
+	$( "button" ).hover(
+		function() {
+			$( this ).addClass( "ui-state-hover" );
+		},
+		function() {
+			$( this ).removeClass( "ui-state-hover" );
+		}
+	);
+	$( "select" ).selectmenu();
+	$( ".accordeon" ).accordion();
+});
