fb1d40bcd60a4a92791b58ff333fc697125d5436
ttacke <ttacke@e6676588-be30-0410-be5a-a11ef2dab0a3>
2015-12-22 00:20:49 +0000

    Liste der Mitspieler und deren Beschreibung korrekt eingebunden
    git-svn-id: https://svn.apps.seibert-media.net/repos/main/ci_job_utils/casino@3014 e6676588-be30-0410-be5a-a11ef2dab0a3

--- htdocs/src/casino.js
+++ htdocs/src/casino.js
@@ -1,32 +1,62 @@
-var anzeige_groesse = 100;
-var interne_bots = [
+var ANZEIGE_GROESSE = 100;
+var INTERNE_BOTS = [
 	new PokerBotVerlierer(),
 	new PokerBotChecker(),
 	new PokerBotNarziss(),
 	new PokerBotYgramul()
 ];
+var MITSPIELER = [
+	[INTERNE_BOTS[0], INTERNE_BOTS[0]],
+	[INTERNE_BOTS[1], INTERNE_BOTS[1]],
+	[INTERNE_BOTS[2], INTERNE_BOTS[1]],
+	[INTERNE_BOTS[3], INTERNE_BOTS[1]],
+	[],
+];
+
 // VOID
 function anzeige_vergroessern() {
-	anzeige_groesse += 10;
-	$('html').attr('style', 'font-size: ' + anzeige_groesse + '%');
+	ANZEIGE_GROESSE += 10;
+	$('html').attr('style', 'font-size: ' + ANZEIGE_GROESSE + '%');
 }
 // VOID
 function anzeige_verkleinern() {
-	anzeige_groesse -= 10;
-	$('html').attr('style', 'font-size: ' + anzeige_groesse + '%');
+	ANZEIGE_GROESSE -= 10;
+	$('html').attr('style', 'font-size: ' + ANZEIGE_GROESSE + '%');
 }
 // VOID
 function befuelle_interne_bots() {
-	var $template = $('#bot_liste .template').remove();
-	var $root = $('#bot_liste');
-	for(var i = 0; i < interne_bots.length; i++) {
-		var content = $template.html();
-		content = content.replace(/\[name\]/, interne_bots[i].name);
-		content = content.replace(/\[beschreibung\]/, interne_bots[i].beschreibung);
-		$root.append(content);//'<li><span class="bot_name">' + interne_bots[i].name + ':</span><span class="bot_beschreibung">' + interne_bots[i].beschreibung + '</span></li>');
+	var beschreibung_template = $('#bot_liste .template').parent().html();
+	$('#bot_liste .template').remove();
+	var $beschreibung = $('#bot_liste');
+	for(var i = 0; i < INTERNE_BOTS.length; i++) {
+		var content = beschreibung_template;
+		content = content.replace(/\[name\]/, INTERNE_BOTS[i].name);
+		content = content.replace(/\[beschreibung\]/, INTERNE_BOTS[i].beschreibung);
+		$beschreibung.append(content);
+	}
+	var mitspieler_template = $('#mitspieler .template').parent().html();
+	$('#mitspieler .template').remove();
+	var $mitspieler = $('#mitspieler');
+	for(var i = 0; i < MITSPIELER.length; i++) {
+		var content = mitspieler_template;
+		content = content.replace(/\[value\]/, i);
+		var text = '';
+		if(MITSPIELER[i].length == 0) {
+			text = 'Keine: freies Spiel, z.B. Turniere';
+		} else {
+			var list = [];
+			for(var j = 0; j < MITSPIELER[i].length; j++) {
+				list.push(MITSPIELER[i][j].name);
+			}
+			text = list.join(' + ');
+		}
+		content = content.replace(/\[text\]/, text);
+		$mitspieler.append(content);
 	}
 }
 $(function() {
+	befuelle_interne_bots();
+	
 	$( "button" ).hover(
 		function() {
 			$( this ).addClass( "ui-state-hover" );
@@ -37,5 +67,5 @@ $(function() {
 	);
 	$( "select" ).selectmenu();
 	$( ".accordeon" ).accordion();
-	befuelle_interne_bots();
+	
 });
