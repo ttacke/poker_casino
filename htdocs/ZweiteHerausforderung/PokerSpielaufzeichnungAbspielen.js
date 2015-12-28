b7ce86f8f9e6c8de4c7ae2259d7522eba3966074
ttacke <ttacke@e6676588-be30-0410-be5a-a11ef2dab0a3>
2015-12-28 22:56:41 +0000

    Board angezeigt
    git-svn-id: https://svn.apps.seibert-media.net/repos/main/ci_job_utils/casino@3047 e6676588-be30-0410-be5a-a11ef2dab0a3

--- htdocs/ZweiteHerausforderung/PokerSpielaufzeichnungAbspielen.js
+++ htdocs/ZweiteHerausforderung/PokerSpielaufzeichnungAbspielen.js
@@ -10,17 +10,37 @@ function PokerSpielaufzeichnungAbspielen() {
 	this.$spielerplaetze_rechts = null;
 	this.spieler_zuordnung = {};
 	this.$karten_template = null;
+	this.$board = null;
 	
 	// VOID
 	this._naechsterZug = function() {
 		var zug = this.spielzuege.shift();
-		var $anzeige = this.spieler_zuordnung[zug.spieler];
 		for(var name in this.spieler_zuordnung) {
 			if(name == zug.spieler) continue;
 			this.spieler_zuordnung[name].removeClass('ist_an_der_reihe');
 		}
-		$anzeige.addClass('ist_an_der_reihe');
+		this._zeige_spielzug(zug);
+	};
+	// VOID
+	this._zeige_spielzug = function(zug) {
+		if(zug.frage.Tisch.length > 0 && this.$board.children().length != zug.frage.Tisch.length) {
+			var vorhandene_karten = this.$board.children().length;
+			for(var i = vorhandene_karten; i < zug.frage.Tisch.length; i++) {
+				var farbe = this._gib_kartenfarbe(zug.frage.Tisch[i]);
+				var t = '<span class="karte ' + farbe + '">' + this.$karten_template.html() + '</span>';
+				t = t.replace(/\[wert\]/, this._gib_kartenwert(zug.frage.Tisch[i]));
+				this.$board.append(t);
+			}
+			
+			var self = this;
+			setTimeout(function() {
+				self._zeige_spielzug(zug, $anzeige);
+			}, 1000);
+			return;
+		}
 		
+		var $anzeige = this.spieler_zuordnung[zug.spieler];
+		$anzeige.addClass('ist_an_der_reihe');
 		this._zeige_karten(zug, $anzeige);
 	};
 	// VOID
@@ -28,7 +48,7 @@ function PokerSpielaufzeichnungAbspielen() {
 		var $handkarten = $anzeige.find('.hand');
 		var self = this;
 		
-		if($handkarten.html() == '' && zug.frage.Hand.length > 0) {
+		if(zug.frage.Hand.length > 0 && $handkarten.children().length != zug.frage.Hand.length) {
 			setTimeout(function() {
 				for(var i = 0; i < zug.frage.Hand.length; i++) {
 					var farbe = self._gib_kartenfarbe(zug.frage.Hand[i]);
@@ -36,10 +56,11 @@ function PokerSpielaufzeichnungAbspielen() {
 					t = t.replace(/\[wert\]/, self._gib_kartenwert(zug.frage.Hand[i]));
 					$handkarten.append(t);
 				}
-				setTimeout(function() { self._zeige_antwort($anzeige, zug.antwort.details) }, 1000);
+				self._zeige_karten(zug, $anzeige);
 			}, 1000);
 			return;
 		}
+		
 		setTimeout(function() { self._zeige_antwort($anzeige, zug.antwort.details) }, 1000);
 	}
 	// STRING
@@ -80,9 +101,6 @@ function PokerSpielaufzeichnungAbspielen() {
 	};
 	// VOID
 	this._erzeuge_spieler = function(spieler) {
-		this.$spielerplaetze_links.html('');
-		this.$spielerplaetze_rechts.html('');
-		
 		var sitzen_links = Math.floor(spieler.length / 2) - 1;
 		for(var i = 0; i < spieler.length; i++) {
 			var t = '<div id="spieleranzeige' + i + '" class="spieler">' + this.$spieler_template.html() + '</spieler>';
@@ -104,12 +122,18 @@ function PokerSpielaufzeichnungAbspielen() {
 		this.$spielerplaetze_rechts = $('#tisch tbody .rechts');
 		this.$spieler_template = $('#spieler_template');
 		this.$karten_template = $('#karte_template');
+		this.$board = $('#board');
+		
+		this.$spielerplaetze_links.html('');
+		this.$spielerplaetze_rechts.html('');
+		this.$board.html('');
 		
 		//TODO hier weiter
 		// -> am ende done melden
 		// -> board leeren und karten dort anzeigen
 		this._erzeuge_spieler(this.showdown_daten.frage.Spieler);
 		// -> einsatz zeigen (muss aus nächstem Zug ermittelt werden - frage.Spieler.Einsatz )
+		// gewinner anzeigen
 		this._naechsterZug();
 		
 		/*Relevante Daten
