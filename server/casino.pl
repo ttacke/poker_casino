f36fefc5a2c71feeae6cd7ad731c6ba6f0fe4632
ttacke <ttacke@e6676588-be30-0410-be5a-a11ef2dab0a3>
2015-12-29 22:16:33 +0000

    Debug via log ermöglicht
    git-svn-id: https://svn.apps.seibert-media.net/repos/main/ci_job_utils/casino@3065 e6676588-be30-0410-be5a-a11ef2dab0a3

--- server/casino.pl
+++ server/casino.pl
@@ -10,22 +10,28 @@ my $PORT = $ARGV[1] || '8080';
 #TODO Tipp von Ben: der Server geht aus, wenn sich jemand disconnected. Es gibt dann keine Meldung
 while(1) {
 	warn "Starte Server auf $IP:$PORT ...";
-	
-	my $vermittlung = LOCAL::Casino->new();
-	Net::WebSocket::Server->new(
-		listen => 8080,
-		on_connect => sub {
-			my ($serv, $conn) = @_;
-			
-			$vermittlung->neueVerbindung($conn);
-			$conn->on(
-				utf8 => sub {
-					my ($conn, $msg) = @_;
-					$vermittlung->neueNachricht($conn, $msg);
-				},
-			);
-		},
-	)->start;
-	
+	eval {
+		my $vermittlung = LOCAL::Casino->new();
+		Net::WebSocket::Server->new(
+			listen => 8080,
+			on_connect => sub {
+				my ($serv, $conn) = @_;
+				
+				$vermittlung->neueVerbindung($conn);
+				$conn->on(
+					utf8 => sub {
+						my ($conn, $msg) = @_;
+						$vermittlung->neueNachricht($conn, $msg);
+					},
+				);
+			},
+		)->start;
+	};
+	my $error = $@;
+	eval {
+		open(my $fh, '>>', '/tmp/casino_server_error.log');
+		print $fh localtime() . "Server gestorben: $@";
+		close($fh);
+	};
 	warn "Server gestorben, neustart!";
 }
